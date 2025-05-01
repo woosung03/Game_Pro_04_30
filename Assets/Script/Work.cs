3c1fc8ab622ba753d1496160d057637a7f3e5f1b
woosung03 <woosungkang2003@gmial.com>
2025-05-01 09:33:38 +0900

    f

---
+++ Assets/Script/Work.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Work : MonoBehaviour
+{
+    private Animator anim;  // Animator component reference
+    void Start()
+    {
+        anim = GetComponent<Animator>();    // 애니 컴포넌트
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) //마우스 좌클
+        {
+            if (UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject() == false)    //
+            {
+                anim.SetTrigger("Click"); //  마우스 좌클 클릭시 애니메이션 실행
+            } 
+        }
+    }
+}
